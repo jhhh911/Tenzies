5de8abd0d85f08b766638b00010afbc0bc04a32c
James Heiniger <james.heiniger911@gmail.com>
2026-01-23 10:14:44 +1030

    Rendering random dice numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import Die from "./Die";
+import { useState } from "react";
 
 export default function App() {
+
+function generateAllNewDice() {
+  return new Array(10)
+    .fill(0)
+    .map(() => Math.ceil(Math.random() * 6))
+}
+
+const [randArray, setRandArray] = useState(generateAllNewDice())
+
+const diceNumbers = randArray.map(die => (
+  <Die value={die} />
+))
+
   return (
     <main>
       <div className="dice-container">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
+        {diceNumbers}
       </div>
     </main>
   );
