ab9cfa5541a4727583e7521eb95173629bbc7591
James Heiniger <james.heiniger911@gmail.com>
2026-01-25 13:44:41 +1030

    Created win condition and can now play a new game.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Die from "./Die";
 import { useState } from "react";
 import { nanoid } from "nanoid";
+import Confetti from 'react-confetti'
 
 export default function App() {
-  const [dice, setDice] = useState(generateAllNewDice());
+  const [dice, setDice] = useState(() => generateAllNewDice());
+
+  const gameWon = dice.every(die => die.isHeld && die.value === dice[0].value)
 
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => ({
@@ -13,18 +16,25 @@ export default function App() {
     }));
   }
 
-  function hold(id) {
-    setDice(dice.map(die => {
-      return die.id === id ? {...die, isHeld: !die.isHeld} : {...die}
-    }))
+  function newGame() {
+    setDice(generateAllNewDice())
   }
 
-function rollDice() {
-  setDice(oldDice => oldDice.map(die =>
-    die.isHeld ? die : {...die, value: Math.ceil(Math.random() * 6)}
-  ))
-}
-  
+  function hold(id) {
+    setDice(
+      dice.map(die => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : { ...die };
+      }),
+    );
+  }
+
+  function rollDice() {
+    setDice(oldDice =>
+      oldDice.map(die =>
+        die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) },
+      ),
+    );
+  }
 
   const diceElements = dice.map(die => (
     <Die
@@ -38,12 +48,15 @@ function rollDice() {
 
   return (
     <main>
+      {gameWon && <Confetti width={1800} />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">{diceElements}</div>
-      <button
-        className="roll-dice"
-        onClick={rollDice}
-      >
-        Roll
+      <button className="roll-dice" onClick={gameWon ? newGame : rollDice}>
+        {gameWon ? 'New Game' : 'Roll'}
       </button>
     </main>
   );
